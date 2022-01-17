f94cb608871ba354d0d75bd4974808caf45247ed
Kaithlyn <kavandeelen@st.noorderpoort.nl>
2022-01-17 14:28:47 +0100

    last push

---
+++ URP GameChallenge2022/Assets/Scripts/ExitGameButton.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitGameButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Debug.Log("exit game");
+            Application.Quit();
+        }
+    }
+    public void ButtonExitGame()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Debug.Log("exit game");
+            Application.Quit();
+        }
+    }
+}
